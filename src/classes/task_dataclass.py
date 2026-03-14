e806da53fffa7fb509f3c8d041947f6b8d71c38b
dropboy27 <pyanzinmihail@gmail.com>
2026-03-14 12:42:32 +0300

    таск

--- src/classes/task_dataclass.py
+++ src/classes/task_dataclass.py
@@ -1,7 +1,20 @@
-from dataclasses import dataclass
+from datetime import datetime
 
-
-@dataclass
 class Task:
-    id: str
-    payload: str
+    def __init__(self, id: int, description: str, priority: int, status: str):
+        self._id = id
+        self._description = description
+        self._priority = priority
+        self.status = status
+        self.created_at = datetime.now()
+
+    @property
+    def id(self):
+        return self._id
+    
+    @property
+    def created_at(self):
+        return self._created_at
+    
+    @property
+    def description(self):
